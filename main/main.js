92216f6398077891f5caa8b123dad2061dcc9ca8
[Zooeeee] <594498431@qq.com>
2018-05-04 14:55:55 +0800

    修改过的

--- main/main.js
+++ main/main.js
@@ -1,4 +1,40 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs){
+//判断函数
+function judge(num){
+    if (num<=2 && num >=0)
+    return fee_1(num);
+    else if (n>2 && n<=8)
+    return fee_2(num);
+    else if(n>8)
+    return fee_3(num);
+    else 
+    return "Error"}
+//取整(四舍五入)
+function int(num){
+    num = num + 0.5;
+    return parseInt(num);
+}
+//加入waiting time 计费
+function add_wait_time(num1,num2){
+return num1 + num2*0.25;
+}
+
+function fee_1(num){
+    return 6 ;
+}
+function fee_2(num){
+    var n = num-2;
+    return n*0.8+6;
+}
+function fee_3(num){
+    var n = num-8;
+    return n*1.2 + 10.8
+}
+
+var n = int(inputs[0]);
+n = judge(n);
+n = add_wait_time(n , inputs[1]);
+n = int(n);
+return n ;
+
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,33 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    var solution1 = [1, 0];
+    var solution2 = [1, 3];
+    var solution3 = [7, 2];
+    var solution4 = [10.4, 5];
+    it('mileage < 2 and no time waiting', function () {
+        var summary = main(solution1);
+        var expected = 6;
+        expect(summary).toEqual(expected);
+    });
+
+    it('mileage < 2 and  time waiting = 3', function () {
+        var summary = main(solution2);
+        var expected = 7;
+        expect(summary).toEqual(expected);
+    });
+
+    it('mileage < 8 and time waiting == 2', function () {
+        let summary = main(solution3);
+        let expected = 11;
+        expect(summary).toEqual(expected);
+    });
+
+    it('mileage > 8 and time waiting == 5', function () {
+        let summary = main(solution4);
+        let expected = 14;
+        expect(summary).toEqual(expected);
+    });
+
 });
+
